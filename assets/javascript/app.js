474d39c07f5cde6195d8bbbaeed679ff0d4c8544
David Luce <david.luce@outlook.com>
2019-06-17 21:56:28 -0600

    validating answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,92 +1,84 @@
 var rightAnswers = 0;
 var wrongAnswers = 0;
 
-
-
 // Game questions & answers
 var myQuestions = [
     {
         // question / answers 1
-        question: "Which 90’s movie featured the Looney Tunes on its soundtrack?",
+        question: "Which 90's movie featured the Looney Tunes on its soundtrack?",
         answers: [
-            'Sapce Jam',
+            'Space Jam',
             'The Looney Toones: Back in action',
             'The Big Lebowski',
-            'Titanic',
+            'Titanic'
         ],
-        correctAnswer: 'Sapce Jam'
+        correctAnswer: 'Space Jam'
     },
     {
         // question / answers 2
-        question: "In 1999, a gallon of gas was?",
-        answers: [
-            '$1.10',
-            '$2.10',
-            '$1.20',
-            '$.99',
-        ],
+        question: 'In 1999, a gallon of gas was?',
+        answers: ['$1.10', '$2.10', '$1.20', '$.99'],
 
         correctAnswer: '$1.20'
-    }, {
+    },
+    {
         // question / answers 3
-        question: "In 2002, VH1 dubbed what 90's song the greatest one-hit wonder of all time?",
+        question:
+            "In 2002, VH1 dubbed what 90's song the greatest one-hit wonder of all time?",
         answers: [
             'Bitter Sweet Symphony - The Verve',
             'Save Tonight - Eagle Eye Chery',
             'Mambo No. 5 - Lou Bega',
-            'Macarena (Bayside Boys Mix) - Los Del Rio',
+            'Macarena (Bayside Boys Mix) - Los Del Rio'
         ],
 
         correctAnswer: 'Macarena (Bayside Boys Mix) - Los Del Rio'
-    }, {
+    },
+    {
         // question / answers 4
-        question: "To get online, you had to use _______ to connect to the internet?",
+        question:
+            'To get online, you had to use _______ to connect to the internet?',
         answers: [
             'A really long extension cord',
             'You called the operator',
             'Dial-Up or NetScape',
-            'Magic Jack',
+            'Magic Jack'
         ],
         correctAnswer: 'Dial-Up or NetScape'
-    }, {
+    },
+    {
         // question / answers 5
-        question: "In 1999, what popular childrens toy did the NSA declare an international threat?",
-        answers: [
-            'Tamagotchi',
-            'Furby',
-            'Nerf Guns',
-            'Gameboy',],
+        question:
+            'In 1999, what popular childrens toy did the NSA declare an international threat?',
+        answers: ['Tamagotchi', 'Furby', 'Nerf Guns', 'Gameboy'],
         correctAnswer: 'Furby'
-
     }
 ];
-console.log(myQuestions)
+// console.log(myQuestions);
 
 // Display question & answers to screen
 function displayQuestion() {
-
     for (i = 0; i < myQuestions.length; i++) {
-        $("#question" + i).html(myQuestions[i].question);
-        var answers = myQuestions[i].answers
+        $('#question' + i).html(myQuestions[i].question);
+        var answers = myQuestions[i].answers;
         for (j = 0; j < answers.length; j++) {
             var newDiv = $('<div>');
             var input = $('<input type="radio" name="answer' + i + '">');
             newDiv.append(input);
             newDiv.append(answers[j]);
 
-            $("#answers" + i).append(newDiv);
-            
+            $('#answers' + i).append(newDiv);
         }
     }
 }
 
 //   start timer
-$("#start").on("click", run);
+$('#start').on('click', run);
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    displayQuestion()
+    displayQuestion();
 }
 
 var intervalId;
@@ -94,29 +86,43 @@ var timer = 60;
 
 function decrement() {
     timer--;
-    $("#timer").text(timer);
+    $('#timer').text(timer);
     if (timer === 0) {
         stop();
-        alert("Times Up!!");
+        alert('Times Up!!');
     }
+};
+
+function checkAnswer(userSelected) {
+    for (var i = 0; i < myQuestions.length; i++) {
+        var correct = myQuestions[i].correctAnswer;
+        console.log(correct)
+
+        if (userSelected[i] === correct) {
+            rightAnswers++;
+            console.log(rightAnswers);
+        } else {
+            wrongAnswers++;
+            console.log(wrongAnswers);
+        }
+    }
+};
+
+$('#stop').on('click', stop);
+function stop() {
+    // var selected = $("#answers").val($("input:checked"));
+    var selected = $('input:checked');
+    //   console.log(selected);
+    //   console.log(selected[0].offsetParent.innerText);
+    for (var i = 0; i < selected.length; i++) {
+        // console.log(selected[i].nextSibling.nodeValue);
+        var userSelected = selected[i].nextSibling.nodeValue;
+        console.log(userSelected);
+    }
+    checkAnswer(userSelected);
+
+    clearInterval(intervalId);
+    // answers[j])
 }
 
-//  The stop function
-$("#stop").on("click", stop);
-
-    function stop() {
-        // var selected = $("#answers").val($("input:checked"));
-        // var selected = $("input:checked");
-        // let question1 = $("input:radio[name=first-name]:checked").val();
-        // console.log(question1)
-        // for (var i = 0; i < selected.length; i++){
-
-        
-        // }
-
-        clearInterval(intervalId);
-        // answers[j])
-    }
-
 // check answers and display right/wrong
-
